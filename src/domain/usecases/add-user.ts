0bd41292f9be3f14ceeb966341aac7ff2b20244d
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-03-19 18:40:26 -0300

    feat: add user domain usecase

---
+++ src/domain/usecases/add-user.ts
@@ -0,0 +1,14 @@
+export interface AddUser {
+  add: (user: AddUser.Params) => Promise<AddUser.Result>
+}
+
+export namespace AddUser {
+  export type Params = {
+    name: string
+    role: string
+    email: string
+    password: string
+  }
+
+  export type Result = boolean
+}
