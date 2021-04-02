9d02cda79f950f4fff7f8f4f4f6da8273c5d4409
Lucas Cotrim <lucascotrim3@hotmail.com>
2021-04-02 11:09:39 -0300

    refactor: change default config port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,7 +3,7 @@ dotenv.config()
 
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/bossabox-backend-challenge',
-  port: process.env.PORT || 5050,
+  port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || 'ASd=123d',
   rootUserName: process.env.ROOT_USER_NAME || 'root',
   rootUserEmail: process.env.ROOT_USER_EMAIL || 'root@root.com',
